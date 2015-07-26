83c6725e2234137a2a102b822ac47ce9a27be650
宇野嵐 <araera48@gmail.com>
2015-07-26 13:46:02 +0900

    プッシュ通知実装

--- app/src/main/java/com/fefeyo/android/comicmarkertalerter/MainActivity.java
+++ app/src/main/java/com/fefeyo/android/comicmarkertalerter/MainActivity.java
@@ -3,6 +3,9 @@ package com.fefeyo.android.comicmarkertalerter;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 
+import com.parse.Parse;
+import com.parse.ParseInstallation;
+
 
 public class MainActivity extends ActionBarActivity {
 
@@ -10,6 +13,9 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Parse.initialize(this, "ECw1iymeuOuX5XyK4I8H0AmHge4XpLLMndgiKhEi", "IVNQ1wmkG6RtrpclNdXGdDT7ChefGHYAHpcyeQhT");
+        ParseInstallation.getCurrentInstallation().saveInBackground();
 
     }
 }
